cab2f274104b60703b3df08d979fd4d7228b3d4e
ya3k <truongdinhvan1233@gmail.com>
2025-03-28 21:45:44 +0700

    feat: apply text editor for tour info

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
-
+import animate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
 export default {
   darkMode: ["class"],
   content: {
@@ -139,5 +140,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate, typography,],
 } satisfies Config;
