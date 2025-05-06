e737017ffbfbed0999d24f3ed5ebcb89b17fcfe8
voconghuy3011 <voconghuy3011@outlook.com.vn>
2025-05-06 22:19:11 +0700

    feat: config firebase messaging

---
+++ src/app/test-fcm/page.tsx
@@ -0,0 +1,10 @@
+import FcmTokenComp from "@/components/firebaseForeground";
+import React from "react";
+
+export default function TestFcmPage() {
+  return (
+    <div>
+      <FcmTokenComp />
+    </div>
+  );
+}
